698a1c5cf373bf563bf44fccfe8864f78d7d7c7c
Praful Rajput <aryanrajput1393@gmail.com>
2026-05-28 23:51:16 +0530

    fix frontend api url and cors

--- Frontend/src/features/auth/pages/ForgotPassword.jsx
+++ Frontend/src/features/auth/pages/ForgotPassword.jsx
@@ -5,7 +5,7 @@ import '../auth.form.scss'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true
 })
 
